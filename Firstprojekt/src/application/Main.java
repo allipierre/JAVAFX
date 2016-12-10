551f2cfb055c48b947faca8750c51ae541aa3608
De-Great Yartey <yarteydegreat2@gmail.com>
2016-12-10 16:03:59 +0000

    Fixed registration issue #1

--- Firstprojekt/src/application/Main.java
+++ Firstprojekt/src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import controller.MyController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -12,11 +13,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/MyView.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Menu.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			//MyController p=new MyController();
+			//p.initialize("/application/MyView.fxml", resources);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
